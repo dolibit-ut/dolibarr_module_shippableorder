ab932d29a8fb048be6bcb00d50c57e2736744ff5
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-10-24 14:53:58 +0200

    abricotage du module

--- config.php
+++ config.php
@@ -1,7 +1,3 @@
 <?php
 		
-	if(is_file('../main.inc.php'))$dir = '../';
-	else  if(is_file('../../../main.inc.php'))$dir = '../../../';
-	else $dir = '../../';
-
-	include($dir."main.inc.php");
+	require('config.default.php');
