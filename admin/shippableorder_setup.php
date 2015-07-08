222380bed5cd035ef4421c4ba916881119aab3ef
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-07-08 14:56:46 +0200

    Unification structure module

--- admin/shippableorder_setup.php
+++ admin/shippableorder_setup.php
@@ -1,23 +1,52 @@
 <?php
-$res=@include("../../main.inc.php");						// For root directory
-if (! $res) $res=@include("../../../main.inc.php");			// For "custom" directory
+/* <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2015 ATM Consulting <support@atm-consulting.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
+/**
+ * 	\file		admin/shippableorder.php
+ * 	\ingroup	shippableorder
+ * 	\brief		This file is an example module setup page
+ * 				Put some comments here
+ */
+// Dolibarr environment
+$res = @include("../../main.inc.php"); // From htdocs directory
+if (! $res) {
+    $res = @include("../../../main.inc.php"); // From "custom" directory
+}
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT . "/core/lib/admin.lib.php";
+require_once '../lib/shippableorder.lib.php';
 dol_include_once('/abricot/includes/class/class.form.core.php');
 
+// Translations
 $langs->load("admin");
-$langs->load('shippableorder@shippableorder');
+$langs->load("shippableorder@shippableorder");
 
-global $db;
+// Access control
+if (! $user->admin) {
+    accessforbidden();
+}
 
-// Security check
-if (! $user->admin) accessforbidden();
-
-$action=GETPOST('action');
-$id=GETPOST('id');
+// Parameters
+$action = GETPOST('action', 'alpha');
 
 /*
- * Action
+ * Actions
  */
 if (preg_match('/set_(.*)/',$action,$reg))
 {
@@ -50,20 +79,32 @@ if (preg_match('/del_(.*)/',$action,$reg))
 /*
  * View
  */
+$page_name = "ShippableOrderSetup";
+llxHeader('', $langs->trans($page_name));
 
-llxHeader('',$langs->trans("ShippableOrderSetup"));
+// Subheader
+$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php">'
+    . $langs->trans("BackToModuleList") . '</a>';
+print_fiche_titre($langs->trans($page_name), $linkback);
 
-$linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
-print_fiche_titre($langs->trans("ShippableOrderSetup"),$linkback,'shippableorder@shippableorder');
+// Configuration header
+$head = shippableorderAdminPrepareHead();
+dol_fiche_head(
+    $head,
+    'settings',
+    $langs->trans("Module104050Name"),
+    0,
+    "shippableorder@shippableorder"
+);
 
-print '<br>';
-
-$var=true;
+// Setup page goes here
+$form=new Form($db);
+$var=false;
 print '<table class="noborder" width="100%">';
 print '<tr class="liste_titre">';
-print '<td width="500">'.$langs->trans("Parameters").'</td>'."\n";
+print '<td>'.$langs->trans("Parameters").'</td>'."\n";
 print '<td align="center" width="20">&nbsp;</td>';
-print '<td align="center" width="300">'.$langs->trans("Value").'</td>'."\n";
+print '<td align="center" width="100">'.$langs->trans("Value").'</td>'."\n";
 
 
 $form=new TFormCore();
@@ -116,7 +157,6 @@ if(!empty($conf->global->SHIPPABLEORDER_GENERATE_SHIPMENT_PDF) && $conf->global-
 
 print '</table>';
 
-// Footer
 llxFooter();
-// Close database handler
+
 $db->close();
