c1978b7067050b5995cfe1c07b3fc89a3bffec36
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-07-08 15:16:05 +0200

    Ajout option pour classer délivrée la commande lors validation expédition

--- admin/shippableorder_setup.php
+++ admin/shippableorder_setup.php
@@ -155,6 +155,20 @@ if(!empty($conf->global->SHIPPABLEORDER_GENERATE_SHIPMENT_PDF) && $conf->global-
 	print '</td></tr>';
 }
 
+// Automatically close order if all product has been sent
+$var=!$var;
+print '<tr '.$bc[$var].'>';
+print '<td>'.$langs->trans("CloseOrderWhenComplete").'</td>';
+print '<td align="center" width="20">&nbsp;</td>';
+print '<td align="right" width="300">';
+print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'">';
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print '<input type="hidden" name="action" value="set_SHIPPABLEORDER_CLOSE_ORDER">';
+print $formdoli->selectyesno("SHIPPABLEORDER_CLOSE_ORDER",$conf->global->SHIPPABLEORDER_CLOSE_ORDER,1);
+print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">';
+print '</form>';
+print '</td></tr>';
+
 print '</table>';
 
 llxFooter();
--- core/modules/modShippableOrder.class.php
+++ core/modules/modShippableOrder.class.php
@@ -87,7 +87,7 @@ class modShippableOrder extends DolibarrModules
 		//							'dir' => array('output' => 'othermodulename'),      // To force the default directories names
 		//							'workflow' => array('WORKFLOW_MODULE1_YOURACTIONTYPE_MODULE2'=>array('enabled'=>'! empty($conf->module1->enabled) && ! empty($conf->module2->enabled)', 'picto'=>'yourpicto@shippableorder')) // Set here all workflow context managed by module
 		//                        );
-		$this->module_parts = array('hooks'=>array('ordercard'),'tpl'=>1);
+		$this->module_parts = array('hooks'=>array('ordercard'),'tpl'=>1,'triggers'=>1);
 
 		// Data directories to create when module is enabled.
 		// Example: this->dirs = array("/shippableorder/temp");
